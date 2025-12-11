5d7dd6c463d411a59e040ee53f1884b26dd7dc34
Jasper Luo <ruiming_luo@ucsb.edu>
2025-12-10 17:02:18 -0800

    update

--- src/dataset_combine.R
+++
@@ -1,8 +0,0 @@
-library(readr)
-library(dplyr)
-
-df1 <- read_csv("query.csv",show_col_types = FALSE)
-df2 <- read_csv("query2.csv",show_col_types = FALSE)
-
-combined <- bind_rows(df1,df2)
-write_csv(combined, "usgs_data.csv")
